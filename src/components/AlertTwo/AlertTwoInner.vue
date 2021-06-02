9dc01ddc978bba5989ba28dade88f7b4ffc1e291
Jason Chafin <jasonchafin@gmail.com>
2021-06-02 13:40:24 -0700

    added Jest

--- src/components/AlertTwo/AlertTwoInner.vue
+++ src/components/AlertTwo/AlertTwoInner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ribbon-wrap">
-    <div :class="'ribbon '+'class'" role="complementary">
+    <div :class="'ribbon ' + 'class'" role="complementary">
       <div class="inner">
         {{ message }}
         <a :href="url">{{ urlMessage }}</a>
--- tests/unit/AlertOne.spec.js
+++ tests/unit/AlertOne.spec.js
@@ -1,18 +1,16 @@
-import { shallMount } from '@vue/test-utils'
-import AlertOne from '@/components/AlertOne'
+import { shallMount } from "@vue/test-utils";
+import AlertOne from "@/components/AlertOne";
 
 describe("AlertOne.vue Test", () => {
-
-    it("should return -1 when the value is not present", () => {
-        const wrapper = shallMount(AlertOne, {
-                propsData: {
-                    items: Array
-                }
-            })
-            // check the name of the component
-        expect(wrapper.vm.$options.name).toMatch('AlertOne')
-            // check if array contains specified value
-        expect(wrapper.vm.$options.name).toContain('items')
-
+  it("should return -1 when the value is not present", () => {
+    const wrapper = shallMount(AlertOne, {
+      propsData: {
+        items: Array,
+      },
     });
+    // check the name of the component
+    expect(wrapper.vm.$options.name).toMatch("AlertOne");
+    // check if array contains specified value
+    expect(wrapper.vm.$options.name).toContain("items");
+  });
 });
